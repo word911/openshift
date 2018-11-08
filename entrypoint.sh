abe92d614c028202e74eba8eee865666215c56cb
word911 <39214163+word911@users.noreply.github.com>
2018-11-08 15:15:29 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,14 @@
-cd /v2raybin
-echo -e "$CONFIG_JSON" > config.json
-if [ "$CERT_PEM" != "$KEY_PEM" ]; then
-  echo -e "$CERT_PEM" > cert.pem
-  echo -e "$KEY_PEM"  > key.pem
+cd /v2ray
+wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
+unzip v2ray.zip 
+if [ ! -f "v2ray" ]; then
+  mv /v2ray/v2ray-v$VER-linux-64/v2ray .
+  mv /v2ray/v2ray-v$VER-linux-64/v2ctl .
+  mv /v2ray/v2ray-v$VER-linux-64/geoip.dat .
+  mv /v2ray/v2ray-v$VER-linux-64/geosite.dat .
 fi
+
+cp -f /config.json .
+chmod +x v2ray v2ctl
+sed -i "s/your_uuid/$UUID/g" config.json
 ./v2ray
